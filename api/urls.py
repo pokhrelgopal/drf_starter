a692efc28042712e545e2e4eaff73bdec2abdb08
pokhrelgopal <pokhrelgopal27@gmail.com>
2023-08-19 21:50:14 +0545

    starter template drf

---
+++ api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+]
+urlpatterns += router.urls
